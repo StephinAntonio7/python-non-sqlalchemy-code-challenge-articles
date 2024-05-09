a736da755d18e301338cd68f88741eb41ccd01bd
Stephin Roberts <stephin.roberts@gmail.com>
2024-05-09 01:05:38 -0400

    done

--- lib/debug.py
+++ lib/debug.py
@@ -4,6 +4,9 @@ import ipdb;
 from classes.many_to_many import Article
 from classes.many_to_many import Author
 from classes.many_to_many import Magazine
+        
+
+
 
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
